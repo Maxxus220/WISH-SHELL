5e78fc602aa14682ec6a11644b5463e2fad9e6e7
Michael Feist <Maxxus220@gmail.com>
2022-09-27 18:46:48 -0500

    Set up wish main loop outline

---
+++ wish.c
@@ -0,0 +1,20 @@
+
+/**
+ * Main shell loop
+ */
+int main(int argc, char *argv[]) {
+    while(1) {
+        // Wait for input
+
+
+        // TODO: Get user input
+
+
+        // TODO: Tokenize user input
+
+
+        // TODO: Run corresponding command and check for valid command
+        
+    }
+    exit(0);
+}
