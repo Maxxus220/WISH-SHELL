ce016c5529e06ab452b9a5f394325f2ffca19928
MICHAEL Feist <feist@royal-02.cs.wisc.edu>
2022-10-08 23:57:02 -0500

    Partial shell_if

--- builtIns.c
+++ builtIns.c
@@ -12,7 +12,6 @@ int shell_cd(char* args[]) {
     return chdir(args[0]);
 }
 
-// TODO: Possibly need to handle whether path needs / at end either error or add it automatically
 int shell_path(char* args[], int argc, char **path, int *path_size) {
     path = realloc(path, argc);
     for(int i = 0; i < argc; i++) {
@@ -25,6 +24,20 @@ int shell_path(char* args[], int argc, char **path, int *path_size) {
 }
 
 int shell_if(char* args[], int argc) {
-    
+    if(strcmp(args[argc-1],"fi") != 0) {
+        return -1;
+    }
+    int i = 1;
+    while(strcmp(args[i],"!=") != 0 && strcmp(args[i],"==") != 0) {
+        i++;
+    }
+    if(strcmp(args[0],"if") == 0) {
+        char* new_args[i] = {0};
+        for(int j = 1; j < i; j++) {
+            new_args[j-1] = malloc(strlen(args[j]));
+            strcpy(new_args[j-1],args[j]);
+        }
+        shell_if(new_args, j);
+    }
     return -1;
 }
